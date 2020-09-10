bafca0b59fe48b354efd157dd79af74d5ffd79ee
deokarmeldeki <dekisiansahan64@gmail.com>
2020-09-10 10:53:32 -0500

    pembuatan class animal (anjing dan elang)

--- index.php
+++ index.php
@@ -42,3 +42,45 @@ echo "Bisa Terbang : $Kucing->bisa_terbang";
 echo "<br>";
 echo "Suara : $Kucing->suara";
 echo "<hr>";
+
+CLASS Anjing{
+		public $nama, $jlh_kaki, $bisa_terbang, $suara;
+
+	}
+
+$Anjing = new Anjing;
+$Anjing->nama ="Guntam";
+$Anjing->jlh_kaki ="4";
+$Anjing->bisa_terbang ="tidak";
+$Anjing->suara ="Guk...Guk...Guk";
+
+echo "Anjing <br>";
+echo "Nama : $Anjing->nama";
+echo "<br>";
+echo "Jumlah Kaki : $Anjing->jlh_kaki";
+echo "<br>";
+echo "Bisa Terbang : $Anjing->bisa_terbang";
+echo "<br>";
+echo "Suara : $Anjing->suara";
+echo "<hr>";
+
+CLASS Elang{
+		public $nama, $jlh_kaki, $bisa_terbang, $suara;
+
+	}
+
+$Elang = new Elang;
+$Elang->nama ="Oby";
+$Elang->jlh_kaki ="4";
+$Elang->bisa_terbang ="tidak";
+$Elang->suara ="Guk...Guk...Guk";
+
+echo "Elang <br>";
+echo "Nama : $Elang->nama";
+echo "<br>";
+echo "Jumlah Kaki : $Elang->jlh_kaki";
+echo "<br>";
+echo "Bisa Terbang : $Elang->bisa_terbang";
+echo "<br>";
+echo "Suara : $Elang->suara";
+echo "<hr>";
