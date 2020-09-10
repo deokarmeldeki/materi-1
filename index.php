41e4e4fb6ddc668d00e793c665314e0e4c874a72
deokarmeldeki <dekisiansahan64@gmail.com>
2020-09-10 10:48:09 -0500

    pembuatan class animal (kucing)

--- index.php
+++ index.php
@@ -21,3 +21,24 @@ echo "Bisa Terbang : $Angsa->bisa_terbang";
 echo "<br>";
 echo "Suara : $Angsa->suara";
 echo "<hr>";
+
+CLASS Kucing{
+		public $nama, $jlh_kaki, $bisa_terbang, $suara;
+
+	}
+
+$Kucing = new Kucing;
+$Kucing->nama ="Flerken";
+$Kucing->jlh_kaki ="4";
+$Kucing->bisa_terbang ="tidak";
+$Kucing->suara ="Miauwww";
+
+echo "Kucing <br>";
+echo "Nama : $Kucing->nama";
+echo "<br>";
+echo "Jumlah Kaki : $Kucing->jlh_kaki";
+echo "<br>";
+echo "Bisa Terbang : $Kucing->bisa_terbang";
+echo "<br>";
+echo "Suara : $Kucing->suara";
+echo "<hr>";
